d3f6989e93e296aac2f1d9bc2a130ccdc521b61d
Marcus Ho <42228958+marcusho21@users.noreply.github.com>
2024-08-22 16:56:22 -0400

    more stuffs

--- server.js
+++ server.js
@@ -6,12 +6,29 @@ const bodyParser = require("body-parser");
 
 server.use(cors());
 server.use(bodyParser.json());
+const mockData = [
+  {
+    Id: 1,
+    Subject: "HighWay Thru Hell",
+    StartTime: "2024-08-22T10:52:00Z",
+    EndTime: "2024-08-22T12:52:00Z",
+  },
+];
+
+server.post("/schedules", (req, res) => {
+  return res.status(200).json(mockData);
+});
 
 // Add custom routes before JSON Server router
-server.post("/schedules", (req, res) => {
-  console.log(req.body);
-  res.status(200);
-  res.jsonp(req.query);
+server.post("/schedules/add", (req, res) => {
+  mockData.push({
+    Id: mockData.length + 1,
+    Subject: "Hello World",
+    StartTime: "2024-08-23T10:52:00Z",
+    EndTime: "2024-08-23T12:52:00Z",
+  });
+
+  return res.status(200).json({ message: "Batch action success" });
 });
 
 // Use default router
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, viewChild, ViewEncapsulation } from '@angular/core';
 import {
-  Component,
-  effect,
-  OnInit,
-  signal,
-  viewChild,
-  ViewEncapsulation,
-} from '@angular/core';
-import {
+  ActionEventArgs,
   AgendaService,
   DayService,
   EventSettingsModel,
-  FieldModel,
   MonthAgendaService,
   MonthService,
   ScheduleComponent,
@@ -21,17 +15,14 @@ import {
   WeekService,
   WorkWeekService,
 } from '@syncfusion/ej2-angular-schedule';
-import {
-  DataManager,
-  Query,
-  UrlAdaptor,
-  WebApiAdaptor,
-} from '@syncfusion/ej2-data';
+import { DataManager, UrlAdaptor, WebApiAdaptor } from '@syncfusion/ej2-data';
+import { extend } from '@syncfusion/ej2-base';
+import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ScheduleModule],
+  imports: [ScheduleModule, JsonPipe, NgIf],
   providers: [
     DayService,
     WeekService,
@@ -54,6 +45,7 @@ export class AppComponent implements OnInit {
     url: 'http://localhost:3000/schedules',
     adaptor: new UrlAdaptor(),
     crossDomain: true,
+    insertUrl: 'http://localhost:3000/schedules/add',
   });
   eventSettings: EventSettingsModel = {
     dataSource: this.dataManager,
@@ -81,11 +73,34 @@ export class AppComponent implements OnInit {
   onAddEvent() {
     const schedule = this.scheduleComponent();
     if (!schedule) return;
-    schedule.openEditor(this.getEventData(schedule), 'Save', true);
+    schedule.openEditor(this.getEventData(schedule), 'Add', true);
   }
 
+  // Intercepts all event actions and other unimportant events when the user interacts with the scheduler and the component loads
+  onActionBegin(args: ActionEventArgs) {
+    console.log(args);
+  }
+
+  // emits when any action is completed successfully
+  onActionComplete(args: ActionEventArgs) {
+    console.log(args);
+    if (args.requestType === 'eventCreated') {
+      this.scheduleComponent()?.refreshEvents();
+    }
+  }
+
+  // actionFailure emits when any event actions fail
+  // error is type unknown because the error object is not documented in the Syncfusion API and it's changed based on the error backend responses
+  // seems like action is always 'actionFailure' because it's the only action that emits this event
+  onActionFailure(args: { error: unknown; name: 'actionFailure' }) {
+    console.log(args);
+  }
+
+  // how to construct an event object to add to the scheduler
   getEventData(schedule: ScheduleComponent) {
     const date: Date = schedule.selectedDate;
+
+    // these have to be upper case for it to work
     return {
       Id: schedule.getEventMaxID(),
       Subject: '',
